9990877761438dc04d1d308dd938532704d0252e
Elliott Forde <eforde@mit.edu>
2018-03-20 18:10:40 -0400

    Fix elliptic curves; finish 1c

--- elliptic-curves.py
+++ elliptic-curves.py
@@ -13,32 +13,61 @@ def add(P, Q):
         lamb = (Q[1] - P[1]) / (Q[0] - P[0])
     x = lamb ** 2 - P[0] - Q[0]
     y = lamb * (P[0] - x) - P[1]
-    return (x, y)
+    # assert point is on curve
+    S = (x, y)
+    assert S[1] ** 2 - (S[0] ** 3 + A * S[0] + B) <= 0.0001
+    return S
+
+
+def mult(n, P):
+    assert n > 0
+    S = P
+    for i in xrange(n - 1):
+        S = add(P, S)
+    # assert point is on curve
+    assert S[1] ** 2 - (S[0] ** 3 + A * S[0] + B) <= 0.0001
+    return S
 
 
 # calculate P + Q
-def calc1():
+def calcb1():
     P = (FFElem(field, 60.), FFElem(field, 22.))
     Q = (FFElem(field, 13.), FFElem(field, 128.))
     S = add(P, Q)
-    assert S[1] ** 2 - (S[0] ** 3 + A * S[0] + B) <= 0.1
     print "P + Q is", S
 
 
 # calculate 100G
-def calc2():
+def calcb2():
     G = (FFElem(field, 292.), FFElem(field, 374.))
-    S = G
-    for i in xrange(100):
-        S = add(G, S)
-    assert S[1] ** 2 - (S[0] ** 3 + A * S[0] + B) <= 0.1
+    S = mult(100, G)
     print "100G is", S
+
+
+def calcc():
+    G = (FFElem(field, 292.), FFElem(field, 374.))
+    S_a = 21
+    S_b = 35
+    P_a = mult(S_a, G)
+    P_b = mult(S_b, G)
+
+    print "Alice sends", P_a
+    print "Bob sends", P_b
+    secret_a = mult(S_a, P_b)
+    secret_b = mult(S_b, P_a)
+    assert secret_a[0] - secret_b[0] <= 0.0001
+    assert secret_a[1] - secret_b[1] <= 0.0001
+    print "Shared secret point is", secret_a
+    print "Shared secret is", secret_a[0]
 
 
 def main():
     print "3-1 (b)"
-    calc1()
-    calc2()
+    calcb1()
+    calcb2()
+
+    print "\n3-1 (c)"
+    calcc()
 
 
 if __name__ == "__main__":
